cf5c8cf71ce82248f33a7879614180ee7427ded4
Meas Sothyro <2021320meas@aupp.edu.kh>
2021-07-03 14:01:13 +0700

    add input age

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 firstname = input("What is your firstname?: ")
 lastname = input ("What is your lastname? : ")
 hobbit = input ("What is your favorit hobbit?: ")
+age = input ("What is your age?: ")
