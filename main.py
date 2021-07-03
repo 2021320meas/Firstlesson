1d32f31ac9fff0af74f8833203fe3547d1c8525c
Meas Sothyro <2021320meas@aupp.edu.kh>
2021-07-03 16:18:02 +0700

    initial git

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
 firstname = input("What is your firstname?: ")
 lastname = input ("What is your lastname? : ")
 hobbit = input ("What is your favorit hobbit?: ")
-age = input ("What is your age?: ")
