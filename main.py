38d2fb301532bdb2b3e26d1458245f263b4163a3
Meas Sothyro <2021320meas@aupp.edu.kh>
2021-07-04 08:40:42 +0700

    revert back to initial change

--- main.py
+++ main.py
@@ -1,5 +1,2 @@
 firstname = input("What is your firstname?: ")
 lastname = input ("What is your lastname? : ")
-hobbit = input ("What is your favorit hobbit?: ")
-age = input(" input your age? : ")
-language = input ("input your languages please? : ")
